a20708cbaa533f3b0b48c4ea1689daa5e41e03c6
KRONOZ006 <therealkronozgmaing@gmail.com>
2025-09-13 13:10:16 -0700

    first

---
+++ src/main/java/net/kronoz/randomstuff/Randomstuff.java
@@ -0,0 +1,10 @@
+package net.kronoz.randomstuff;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Randomstuff implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/net/kronoz/randomstuff/client/RandomstuffClient.java
@@ -0,0 +1,10 @@
+package net.kronoz.randomstuff.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomstuffClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
